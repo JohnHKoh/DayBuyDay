6bf0fe9bcf0119856dad6f87972bc301d739aa4f
John Koh <jhkoh16@gmail.com>
2018-03-14 17:05:55 -0400

    Commit all htdocs

--- login.js
+++ login.js
@@ -1,6 +1,18 @@
 function checkCreated() {
     var check = localStorage.getItem("created");
+	console.log(check);
     if (check === "true") {
         document.getElementById('created').style.display = "block";
+		localStorage.setItem("created", "false");
     }
+	else if (check === "dup") {
+		document.getElementById('created').style.display = "block";
+		document.getElementById('msg').style.color = "red";
+		document.getElementById('msg').innerHTML = "Email already in use.";
+		localStorage.setItem("created", "false");
+    }
+	else {
+		document.getElementById('created').style.display = "none";
+		localStorage.setItem("created", "false");
+	}
 }
