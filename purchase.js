989251eec661a43cdeec7c04438b5f383ec904ba
John Koh <jhkoh16@gmail.com>
2018-03-19 20:43:59 -0400

    Fixed payment radio buttons

--- purchase.js
+++ purchase.js
@@ -1,8 +1,8 @@
 function changeButton(option) {
     if (option === 'day') {
-        document.getElementById('payButton').value = "7/f26+/YTFW63Wx80y4/F8nTTZaNCE4vdA1j1xBsJ9Cxsm+talKFmKdTilYvzTL6Zpc6vqZkGI/Sb2SdO8HS8Blw7TmEux+cP0cjLinUYH7sGt+ID0tvnp3sj95j5eRJTCwHC9G1qLM8T8fVXbFNj1h3Pm3ZLVCgNv81iuvPxyOhoxvBZyGstnCHvbrp0Rs5MUnQnuuw8zmEB5p9KjOopvJ+7LrLRInhqxD8LAt0Ejn6q2SYmjC7wfE5UbKPv2Fz";
+        document.getElementById('payButton').value = "7/f26+/YTFW63Wx80y4/F8nTTZaNCE4vdA1j1xBsJ9Cxsm+talKFmKdTilYvzTL6OxNNk0pUGVTkTX2WnhA3MvGKV+LEhJgz4VxKCniBHXfUW1L422KHaWiy0ikgdk7icX8MOiVvW3x5auJhMIRtfBsX0fkXAQjGWahZi+Igr41ZQl72tIbpx/YEf/kiSvaIPyForGerkB1/waCRRy2fD/Ruxv87PD36ZOkjehvhpmzLSTmUcYInjU/ML/JllqVF";
     }
     if (option === 'week') {
-        document.getElementById('payButton').value = "7/f26+/YTFW63Wx80y4/F8nTTZaNCE4vdA1j1xBsJ9Cxsm+talKFmKdTilYvzTL6QYQ8jx/JcDssvVQ0iK7AG6Xab0NlbPdNp8gWYdKDIuyZIRxNvttk6jJKlR8p6MjMtTiMBcmHrHzE5hKpgJI50kDkRWrcDMF8wdi/4l8TGOIuZ4F0UMXT18BFdY4zLMojXx02H9mXg7mKCbjSgBUNdMbrfJMBcJ7ShMLuP2AKU1hmhh8fT1EJU0Q5qPDb+M+r";
+        document.getElementById('payButton').value = "7/f26+/YTFW63Wx80y4/F8nTTZaNCE4vdA1j1xBsJ9Cxsm+talKFmKdTilYvzTL6sZjOnS7HHhew3inWJr4StJKrsWeEJ/TtcnDWMLHeucbBrwTOJPccXN6s/AYp6X8HaBBfKW/V+tO/KxQyTVxXlAvFDWn7CoSRlPn+AMhaHrITvBJKcyeAWkbOVx+bsPBqj0tUy+NxjinooCBGQ2A/ty8NaOaZqtlPDcgT9NBoDV38SOpZq6mfnRb/ckuvlaH6";
     }
 }
