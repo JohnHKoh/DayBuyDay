049c716105f9aa7b9ebca9f7a7a6adbd95e5e35a
John Koh <jhkoh16@gmail.com>
2018-04-07 18:12:40 -0400

    Hacky css fix for members page

--- member.php
+++ member.php
@@ -32,7 +32,7 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/members.css" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
         <script src="purchaseMember.js"></script>
 	</head>
@@ -117,7 +117,7 @@
                                         <input type="image" src="https://test.bitpay.com/img/button-large.png" border="0" name="submit" alt="BitPay, the easy way to pay with bitcoins." >
                                     </form>
 								</header>		
-				<a href="login.html" class="button" style="margin-bottom:3cm;">Logout</a>
+				<a href="login.html" class="button" style="margin-bottom: 50px">Logout</a>
 							</article>
                         </div>
                     </div>
